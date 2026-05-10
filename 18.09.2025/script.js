c50bdb9fa497201d6253c6ff8b1dafa57f8723aa
balddrAdmin <boliklevik@gmail.com>
2026-05-11 00:00:45 +0200

    18.09.2025 6

--- 18.09.2025/script.js
+++ 18.09.2025/script.js
@@ -4,7 +4,14 @@ const container = document.querySelector('#polakiahueli')
 
 for(let i = 1; i <= 31; i++){
     const yacheika = document.createElement('div')
-    yacheika.textContent = 'хуета'
+
+    const data = document.createElement('h5')
+    data.textContent = `2020-08-${String(i).padStart(2, "0")}`
+    yacheika.appendChild(data)
+
+    const text = document.createElement('p')
+    yacheika.appendChild(text)
+
     yacheika.className = 'yacheika'
     container.appendChild(yacheika)
 }
