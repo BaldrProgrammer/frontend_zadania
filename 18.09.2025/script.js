c488f831d2e97d5e5ca2980c1e9b3848269bb74e
balddrAdmin <boliklevik@gmail.com>
2026-05-11 22:16:30 +0200

    18.09.2025 7

--- 18.09.2025/script.js
+++ 18.09.2025/script.js
@@ -1,9 +1,17 @@
 const input = document.querySelector('#polakipidory_i')
 const button = document.querySelector('#polakipidory_b')
 const container = document.querySelector('#polakiahueli')
+let selected = null;
 
 for(let i = 1; i <= 31; i++){
     const yacheika = document.createElement('div')
+    yacheika.addEventListener('click', ()=>{
+        yacheika.style.outline = '2px solid red'
+        if (selected){
+            selected.style.outline = '1px solid #7DA453'
+        }
+        selected = yacheika
+    })
 
     const data = document.createElement('h5')
     data.textContent = `2020-08-${String(i).padStart(2, "0")}`
@@ -15,3 +23,7 @@ for(let i = 1; i <= 31; i++){
     yacheika.className = 'yacheika'
     container.appendChild(yacheika)
 }
+
+button.addEventListener('click', ()=>{
+    selected.querySelector('p').textContent = input.value
+})
