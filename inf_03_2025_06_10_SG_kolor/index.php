d7e9e1a512719b140abe5f09ce058fb78de21adf
balddrAdmin <boliklevik@gmail.com>
2026-06-01 19:27:43 +0200

    inf03 2zad index.php left section

---
+++ inf_03_2025_06_10_SG_kolor/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE 'html'>
+<head>
+    <meta lang="pl">
+    <title>Szkolenia i kursy</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+<header>
+    <h1>SZKOLENIA</h1>
+</header>
+<main>
+    <section class="left">
+        <table>
+            <thead>
+            <tr>
+                <td>Kurs</td>
+                <td>Nazwa</td>
+                <td>Cena</td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+
+            </tr>
+            </tbody>
+        </table>
+    </section>
+    <section class="right">
+
+    </section>
+</main>
+<footer>
+
+</footer>
+</body>
