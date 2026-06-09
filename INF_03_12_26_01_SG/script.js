334fe7f30726b765db39e27793763e51126e913a
balddrAdmin <boliklevik@gmail.com>
2026-06-09 20:17:52 +0200

    inf03 zad3 js completed

--- INF_03_12_26_01_SG/script.js
+++ INF_03_12_26_01_SG/script.js
@@ -1,4 +1,14 @@
 SUBMIT = document.getElementById("submit")
+WZOR = document.getElementById("wzorI")
+KOLOR = document.getElementById("kolorS")
+CENA = document.getElementById("cenaI")
+GALERIA = document.getElementById("galeria")
+
 SUBMIT.addEventListener('click', ()=>{
-    console.log('нажато')
+    fileName = WZOR.value.split('\\').at(-1)
+    window.alert(`Wzór: ${fileName}, kolor ${KOLOR.value} w cenie ${CENA.value} zł`)
+    newImg = document.createElement('img')
+    newImg.src = URL.createObjectURL(WZOR.files[0])
+    newImg.setAttribute('class', 'miniatury')
+    GALERIA.appendChild(newImg)
 })
